2b8059de97ac386b3601127e64c1680d620c516e
K Seki <seki@konan-u.ac.jp>
2022-03-31 23:04:07 +0900

    Change variable name

--- run2.sh
+++ run2.sh
@@ -11,6 +11,6 @@ module load python/3.6/3.6.12 cuda/10.0/10.0.130.1 cudnn/7.6/7.6.5
 source venv/bin/activate
 
 #model=transformer_iwslt_de_en_nag
-model=transformer_nag
+dir=transformer_nag
 
-fairseq-generate data-bin/transcript.tokenized.1m --path checkpoints/$model/checkpoint_best.pt --batch-size 128 --beam 5 --remove-bpe
+fairseq-generate data-bin/transcript.tokenized --path checkpoints/$dir/checkpoint_best.pt --batch-size 128 --beam 5 --remove-bpe
